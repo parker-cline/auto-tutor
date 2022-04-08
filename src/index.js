9869bade8dcbe3e8c68274adba04d96635affe49
Parker Cline <parkerpawleysisland@gmail.com>
2022-04-08 18:59:08 +0100

    basic dialogue tree

--- src/index.js
+++ src/index.js
@@ -1,116 +1,87 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import YarnBound from 'yarn-bound';
 
-function Square(props) {
-  return (
-    <button className="square" onClick={props.onClick}>
-      {props.value}
-    </button>
-  );
-}
-
-class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    };
-  }
-
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext,
-    });
-  }
-
-  renderSquare(i) {
-    return (
-      <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
-      />
-    );
-  }
-
-  render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+const dialogue = ` 
+  title: Start
+  ---
+  Where are ou?
+  Hello, world!
+  That's it!
+  sjkhdaophfopa
+  hdksaophkspodh
+  ===
+`
+class Dialogue extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            runner: new YarnBound({
+                dialogue
+            })
+        };
+        console.log(this.state.runner);
     }
 
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
+    advanceDialogue() {
+        this.state.runner.advance()
+        this.setState({ runner: this.state.runner });
+    }
+    
+    render() {
+        return (
+            <div>
+                <h1>{this.state.runner.currentResult.text}</h1>
+                <button onClick={() => this.advanceDialogue()}>Next</button>
+            </div>
+        );
+    }
 }
-
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
+class Prompt extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        value: '',
+        };
+    }
+    
+    handleChange = (event) => {
+        this.setState({ value: event.target.value });
+    }
+    
+    handleSubmit = (event) => {
+        alert('A name was submitted: ' + this.state.value);
+        event.preventDefault();
+    }
+    
+    render() {
+        return (
+        <form onSubmit={this.handleSubmit}>
+            <label>
+            Name:
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+            </label>
+            <input type="submit" value="Submit" />
+        </form>
+        );
+    }
+}
+class DialogueTree extends React.Component {
+    render() {
+        return (
+            <div className="dialogue">
+                <h1>Hello!</h1>
+                <Dialogue />
+                <Prompt />
+            </div>
+        );
+    }
 }
 
 // ========================================
 
 ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
+    <DialogueTree />,
+    document.getElementById('root')
 );
 
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
