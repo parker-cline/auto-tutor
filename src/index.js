41d1e20b9221fe976ae7b551848548b8ed733b23
Parker Cline <parkerpawleysisland@gmail.com>
2022-04-11 17:30:04 +0100

    multiple dialogue options now display for the user

--- src/index.js
+++ src/index.js
@@ -3,12 +3,30 @@ import ReactDOM from 'react-dom';
 import YarnBound from 'yarn-bound';
 
 const dialogue = ` 
-  title: Start
-  ---
-  Where are ou?
-  Hello, world!
-  That's it!
-  ===
+title: Start
+---
+<<set $shapes_like_you to true>>
+Sphere: Hello, I am Blue Sphere.
+Cube: Hi there Sphere! I'm Red Cube.
+Sphere: And who is this then?
+
+-> I'm Capsule, but my friends call me "Tic Tac". No idea why...
+    <<set $name to "Tic Tac">>
+-> The name's Triquandle.
+    <<set $name to "Triquandle">>
+-> Pyramid. Why; who wants to know?
+    <<set $name to "Pyramid">>
+    <<set $shapes_like_you to false>>
+
+<<if $shapes_like_you>>
+    Sphere: Nice to meet you {$name}!
+    Cube: Yeah, likewise!
+<<else>>
+    Sphere: No need to be so rude...
+    Cube: Yeah, maybe you should be called Grumpy {$name}.
+    Sphere: Ha! Totally.
+<<endif>>
+===
 `
 class Dialogue extends React.Component {
     constructor(props) {
@@ -16,23 +34,48 @@ class Dialogue extends React.Component {
         this.state = {
             runner: new YarnBound({
                 dialogue
-            })
-        };
+            }),
+            dialogueText: '',
+            dialogueOptions: [],
+        }
         console.log(this.state.runner);
     }
 
     advanceDialogue() {
         this.state.runner.advance()
         this.setState({ runner: this.state.runner });
+        console.log(this.state.runner);
     }
     
     render() {
-        return (
-            <div>
-                <h1>{this.state.runner.currentResult.text}</h1>
-                <button onClick={() => this.advanceDialogue()}>Next</button>
-            </div>
-        );
+        if (this.state.runner.currentResult.text) {
+            return (
+                <div>
+                    <h1>{this.state.runner.currentResult.text}</h1>
+                    <button onClick={() => this.advanceDialogue()}>Next</button>
+                </div>
+            );
+        }
+        else if (this.state.runner.currentResult.options) {
+            console.log(this.state.runner.currentResult.options);
+            const listItems = this.state.runner.currentResult.options.map((dialogueChoice, index) =>
+                <li key={index}>{dialogueChoice.text}</li>
+            );
+            return (
+                <div>
+                    <h1>Choose an option.</h1>
+                    <ul>{listItems}</ul>
+                </div>
+            );
+
+        }
+        else {
+            return (
+                <div>
+                    <h1>All done!</h1>
+                </div>
+            );
+        }
     }
 }
 class Prompt extends React.Component {
