8398ccbb041c1605d0dbc8b9017a4c00aadc4d43
Parker Cline <parkerpawleysisland@gmail.com>
2022-09-21 13:28:02 -0300

    refactored to put dialogueText into state

--- src/index.js
+++ src/index.js
@@ -17,34 +17,27 @@ class Dialogue extends React.Component {
     }
 
     advanceDialogue(option=null) {
-        this.state.runner.advance(option)
+        this.state.runner.advance(option);
         this.setState({ runner: this.state.runner });
-    }
-    
-    render() {
         const currPage = this.state.runner.currentResult;
         if (currPage.text) {
             if (currPage.markup.length > 1) {
                 const tagDetails = currPage.markup[1];
                 const slicedString = currPage.text.slice(tagDetails.position, tagDetails.position + tagDetails.length);
-                const dialogueText = reactStringReplace(currPage.text, slicedString, (match, i) => (
-                    <span key={i} style={{ color: 'red' }}>{match}</span>
-                ));
-                return (
-                    <div>
-                        <h3>{currPage.markup[0].properties.name}: {dialogueText}</h3>
-                        <button onClick={() => this.advanceDialogue()}>Next</button>
-                    </div>
-                );
+                this.setState({
+                    dialogueText: reactStringReplace(currPage.text, slicedString, (match, i) => (
+                        <span key={i} style={{ color: 'red' }}>{match}</span>
+                    ))
+                });
+            } else {
+                this.setState({ dialogueText: currPage.text });
             }
-            return (
-                <div>
-                    <h3>{currPage.markup[0].properties.name}: {currPage.text}</h3>
-                    <button onClick={() => this.advanceDialogue()}>Next</button>
-                </div>
-            );
         }
-        else if (currPage.options) {
+    }
+    
+    render() {
+        const currPage = this.state.runner.currentResult;
+        if (currPage.options) {
             const listItems = currPage.options.map((dialogueChoice, index) =>
                 <li key={index} onClick={() => this.advanceDialogue(index)}>{dialogueChoice.text}</li>
             );
@@ -54,12 +47,17 @@ class Dialogue extends React.Component {
                     <ul>{listItems}</ul>
                 </div>
             );
-
-        }
-        else {
+        } else if (currPage.text) {
             return (
                 <div>
-                    <h1>All done!</h1>
+                    <h3>{currPage.markup[0].properties.name}: {this.state.dialogueText}</h3>
+                    <button onClick={() => this.advanceDialogue()}>Next</button>
+                </div>
+            );
+        } else {
+            return (
+                <div>
+                    <h1>End of dialogue.</h1>
                 </div>
             );
         }
