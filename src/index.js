1f6768beb1fd07adfaa2448550fe658d84b3ae9a
Parker Cline <parkerpawleysisland@gmail.com>
2022-11-10 01:58:29 -0300

    refactoring; lifting state "up" to dialogue

--- src/index.js
+++ src/index.js
@@ -2,74 +2,81 @@ import React from 'react';
 import { useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import YarnBound from 'yarn-bound';
-import { dialogue } from './dialogue2.js';
+import { dialogue } from './lessons/lesson2.js';
 import reactStringReplace from 'react-string-replace';
 import 'animate.css';
+/*
 import useSound from 'use-sound';
 import trumpetSound from './assets/sounds/trumpets.mp3';
+import crystalLoopSound from './assets/sounds/crystalloop.wav';
+import crystalGlassSound from './assets/sounds/crystalglass.wav';
+*/
 
 function Dialogue() {
 
-    const generateDialogue = () => {
-        const currPage = runner.currentResult;
-        if (currPage.text) {
-            if (currPage.markup.length > 1) {
-                const tagDetails = currPage.markup[1];
-                const slicedString = currPage.text.slice(tagDetails.position, tagDetails.position + tagDetails.length);
-                return reactStringReplace(currPage.text, slicedString, (match, i) => (
-                    <span key={i} className="animate__animated animate__pulse" style={{ color: tagDetails.name, display: 'inline-block' }}>{match}</span>
-                ))
-            } else {
-                return currPage.text
-            }
+    const advanceDialogue = (option = null) => {
+        runner.advance(option);
+        setDialogueText(generateDialogue(runner.currentResult));
+    }
+
+    const generateDialogue = (currPage) => {
+        if (!currPage.text) {
+            return null;
+        }
+        if (currPage.markup.length > 1) {
+            const tagDetails = currPage.markup[1];
+            const slicedString = currPage.text.slice(tagDetails.position, tagDetails.position + tagDetails.length);
+            return reactStringReplace(currPage.text, slicedString, (match, i) => (
+                <span key={i} className="animate__animated animate__pulse" style={{ color: tagDetails.name, display: 'inline-block' }}>{match}</span>
+            ))
+        } else {
+            return currPage.text
         }
     }
 
     const [runner, setRunner] = useState(new YarnBound({ dialogue }));
-    const [dialogueText, setDialogueText] = useState(generateDialogue());
-    const [playTrumpet] = useSound(trumpetSound);
-
-    const advanceDialogue = (option=null) => {
-        runner.advance(option);
-        setRunner(runner);
-        setDialogueText(generateDialogue());
-    }
-
-    const currPage = runner.currentResult;
-    if (currPage.options) {
-        const listItems = currPage.options.map((dialogueChoice, index) =>
-            <li key={index} onClick={() => advanceDialogue(index)}>{dialogueChoice.text}</li>
-        );
+    const [dialogueText, setDialogueText] = useState(generateDialogue(runner.currentResult));
+    
+    if (dialogueText) {
         return (
-            <div>
-                <h1>Choose an option.</h1>
-                <ul>{listItems}</ul>
-            </div>
-        );
-    } else if (currPage.text) {
-        return (
-            <div>
-                <h3>{currPage.markup[0].properties.name}: {dialogueText}</h3>
-                <button onClick={() => advanceDialogue()}>Next</button>
-            </div>
+            <DialogueText currPage={runner.currentResult} dialogueText={dialogueText} advanceDialogue={advanceDialogue} />
         );
     } else {
         return (
-            <div>
-                <h1>End of dialogue.</h1>
-            </div>
+            <DialogueList currPage={runner.currentResult} advanceDialogue={advanceDialogue} />
         );
     }
 }
 
-class DialogueTree extends React.Component {
-    render() {
-        return (
-            <div className="dialogue">
-                <Dialogue />
-            </div>
-        );
-    }
+function DialogueText({ currPage, dialogueText, advanceDialogue }) {
+    return (
+        <div>
+            <h3>{currPage.markup[0].properties.name}: {dialogueText}</h3>
+            <button onClick={() => advanceDialogue()}>Next</button>
+        </div>
+    );
+}
+
+
+function DialogueList({ currPage, advanceDialogue }) {
+    const listItems = currPage.options.map((dialogueChoice, index) =>
+        <li key={index} onClick={() => advanceDialogue(index)}>{dialogueChoice.text}</li>
+    );
+    return (
+        <div>
+            <h1>Choose an option.</h1>
+            <ul>{listItems}</ul>
+        </div>
+    );
+}
+
+
+function DialogueTree() {
+    return (
+        <div>
+            <Dialogue />
+        </div>
+    );
 }
 
 // ========================================
