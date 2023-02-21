df2e27716edad7d6673c8a9507c8b1a7cb8ace28
Parker Cline <parkerpawleysisland@gmail.com>
2023-02-21 16:30:36 +0800

    eraser mode activated

--- src/index.js
+++ src/index.js
@@ -23,6 +23,36 @@ import "./index.css";
 
 addStyles()
 
+/* NavBar */
+function NavBar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-success bg-gradient">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="/">Lesson</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link" href="/">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="/lesson">Lesson</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/bookmarks">Bookmarks</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+/* Drawing Canvas */
+
 function DrawingCanvas() {
     const [drawing, setDrawing] = useState(false);
     const canvasRef = useRef(null);
@@ -34,10 +64,12 @@ function DrawingCanvas() {
         ctxRef.current.moveTo(offsetX, offsetY);
         setDrawing(true);
     };
+
     const stopDraw = () => {
         ctxRef.current.closePath();
         setDrawing(false);
     };
+
     const draw = ({ nativeEvent }) => {
         if (!drawing) return;
         const { offsetX, offsetY } = nativeEvent;
@@ -85,18 +117,114 @@ function DrawingCanvas() {
     );
 }
 
-function ImageCard({ imgSrc, captionName }) {
+function CanvasEditor({ handleClearCanvas, handleChangeColor }) {
     return (
-        <div className="col-sm-6">
-            <div className="card lesson-column">
-                <img className="card-img-top" src={imgSrc} alt="Placeholder" />
-                <div className="card-body">
-                    <p className="card-text">{captionName}</p>
-                </div>
-            </div>
-        </div>
+        <>
+            <button type="button" className="btn btn-success col-sm-3" onClick={handleClearCanvas}><i className="bi bi-trash"></i> Clear</button>
+            <button type="button" className="btn btn-danger col-sm-3" onClick={() => handleChangeColor('red')}>Change to Red</button>
+            <button type="button" className="btn btn-primary col-sm-3" onClick={() => handleChangeColor('blue')}>Change to Blue</button>
+            <button type="button" className="btn btn-primary col-sm-3" onClick={() => handleChangeColor('white')}>Eraser</button>
+        </>
     );
 }
+
+/* Customize Page */
+
+function Customize() {
+    const [a, setA] = useState('1');
+    const [b, setB] = useState('1');
+    const [c, setC] = useState('1');
+    const [functionType, setFunctionType] = useState('quadratic');
+    const [xBounds, setXBounds] = useState([-5, 5])
+    const [yBounds, setYBounds] = useState([-5, 5])
+
+    useEffect(() => {
+        document.getElementById('error').innerHTML = '';
+        try {
+            functionPlot({
+                target: '#graph',
+                data: [{
+                    fn: functionType === 'quadratic' ? `${a}x^2 + ${b}x + ${c}` : `${a}x + ${b}`,
+                }],
+                grid: true,
+                xAxis: { domain: xBounds },
+                yAxis: { domain: yBounds },
+            });
+        } catch {
+            document.getElementById('error').innerHTML = 'Invalid function.';
+        }
+    }, [a, b, c, xBounds, yBounds, functionType])
+
+
+    const isValidEquation = (type, coeffs) => {
+        if (type === 'quadratic') {
+            return isValidQuadraticEquation(coeffs[0], coeffs[1], coeffs[2])
+        }
+        if (type === 'linear') {
+            return isValidLinearEquation(coeffs[0], coeffs[1])
+        }
+        return false
+    }
+
+    const isValidLinearEquation = (a, b) => {
+        return (b > 0 && -1 * b / a > 0)
+    }
+
+    const isValidQuadraticEquation = (a, b, c) => {
+        const first_root = (-1 * b + Math.sqrt(b * b - 4 * a * c)) / (2 * a)
+        const second_root = (-1 * b - Math.sqrt(b * b - 4 * a * c)) / (2 * a)
+        return (first_root > 0 || second_root > 0) && (c > 0)
+    }
+
+    const navigate = useNavigate();
+    const handleStartLesson = () => {
+        const functionString = functionType === 'quadratic' ? `${a}x^2 + ${b}x + ${c}` : `${a}x + ${b}`;
+        navigate('/lesson', { state: { functionType: functionType, functionString: functionString, xBounds: xBounds, yBounds: yBounds } });
+    }
+
+    return (
+        <>
+            <NavBar />
+            <div className="function-selector-screen">
+                <h1>Choose the type of equation</h1>
+                <input className="btn-check" type="radio" id="quadratic" name="functionType" value="quadratic" checked={functionType === 'quadratic'} onChange={(e) => setFunctionType(e.target.value)} />
+                <label htmlFor="quadratic" className="btn btn-success">Quadratic</label>
+                <input className="btn-check" type="radio" id="linear" name="functionType" value="linear" checked={functionType === 'linear'} onChange={(e) => setFunctionType(e.target.value)} />
+                <label htmlFor="linear" className="btn btn-success">Linear</label>
+                <h1>Enter the equation you want to plot</h1>
+                <StaticMathField>{'y ='}</StaticMathField>
+                <EditableMathField
+                    latex={a}
+                    onChange={(mathField) => {
+                        setA(mathField.latex())
+                    }}
+                />
+                <StaticMathField>{functionType === 'quadratic' ? 'x^2 +' : 'x +'}</StaticMathField>
+                <EditableMathField
+                    latex={b}
+                    onChange={(mathField) => {
+                        setB(mathField.latex())
+                    }}
+                />
+                {functionType === 'quadratic' &&
+                    <>
+                        <StaticMathField>{'x +'}</StaticMathField>
+                        <EditableMathField
+                            latex={c}
+                            onChange={(mathField) => {
+                                setC(mathField.latex())
+                            }}
+                        />
+                    </>}
+                <div id="graph"></div>
+                <div id="error"></div>
+                <button className="btn btn-primary" onClick={handleStartLesson} disabled={!isValidEquation(functionType, [a, b, c])}>Start Lesson</button>
+            </div>
+        </>
+    )
+}
+
+/* Function Plotter */
 
 function FunctionPlot({ functionString, xBounds, yBounds }) {
     const [windowSize, setWindowSize] = useState([
@@ -138,43 +266,7 @@ function FunctionPlot({ functionString, xBounds, yBounds }) {
 
 }
 
-function CanvasEditor({ handleClearCanvas, handleChangeColor }) {
-    return (
-        <>
-            <button type="button" className="btn btn-success col-sm-3" onClick={handleClearCanvas}><i className="bi bi-trash"></i> Clear</button>
-            <button type="button" className="btn btn-danger col-sm-3" onClick={() => handleChangeColor('red')}>Change to Red</button>
-            <button type="button" className="btn btn-primary col-sm-3" onClick={() => handleChangeColor('blue')}>Change to Blue</button>
-        </>
-    );
-}
-
-function NavBar() {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-success bg-gradient">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="/">Lesson</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <a className="nav-link" href="/">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="/lesson">Lesson</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/bookmarks">Bookmarks</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    );
-}
-
+/* Chatting and dialogue  */
 function ChatBox({ chatMessages }) {
     return (
         <div className="col-sm-6">
@@ -189,7 +281,6 @@ function ImageDisplayer({ img_string }) {
     }
     return <img src={require('./assets/images/' + img_string)} className="img-fluid" alt={img_string} />
 }
-
 
 function Dialogue({ dialogueItem, functionString, functionType, xBounds, yBounds }) {
 
@@ -276,8 +367,6 @@ function Dialogue({ dialogueItem, functionString, functionType, xBounds, yBounds
     const runner = new YarnBound({ dialogue: dialogueItem });
 
     const [runnerHistory, setRunnerHistory] = useState(initializeHistory(runner));
-    //const imgSrc = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/440px-Image_created_with_a_mobile_phone.png";
-    //<ImageCard imgSrc={imgSrc} captionName="test" />
     return (
         <>
             <FunctionPlot functionString={functionString} xBounds={xBounds} yBounds={yBounds} />
@@ -285,6 +374,9 @@ function Dialogue({ dialogueItem, functionString, functionType, xBounds, yBounds
         </>
     );
 }
+
+
+/* The Lesson Page */
 
 function Lesson() {
     const lessonInfo = useLocation().state;
@@ -301,105 +393,7 @@ function Lesson() {
     );
 }
 
-function Customize() {
-    const [a, setA] = useState('1');
-    const [b, setB] = useState('1');
-    const [c, setC] = useState('1');
-    const [functionType, setFunctionType] = useState('quadratic');
-    const [xBounds, setXBounds] = useState([-5, 5])
-    const [yBounds, setYBounds] = useState([-5, 5])
-
-    useEffect(() => {
-        document.getElementById('error').innerHTML = '';
-        try {
-            functionPlot({
-                target: '#graph',
-                data: [{
-                    fn: functionType === 'quadratic' ? `${a}x^2 + ${b}x + ${c}` : `${a}x + ${b}`,
-                }],
-                grid: true,
-                xAxis: { domain: xBounds },
-                yAxis: { domain: yBounds },
-            });
-        } catch {
-            document.getElementById('error').innerHTML = 'Invalid function.';
-        }
-    }, [a, b, c, xBounds, yBounds, functionType])
-
-
-    const isValidEquation = (type, coeffs) => {
-        if (type === 'quadratic') {
-            return isValidQuadraticEquation(coeffs[0], coeffs[1], coeffs[2])
-        }
-        if (type === 'linear') {
-            return isValidLinearEquation(coeffs[0], coeffs[1])
-        }
-        return false
-    }
-
-    const isValidLinearEquation = (a, b) => {
-        return (b > 0 && -1 * b / a > 0)
-    }
-
-    const isValidQuadraticEquation = (a, b, c) => {
-        const first_root = (-1 * b + Math.sqrt(b * b - 4 * a * c)) / (2 * a)
-        const second_root = (-1 * b - Math.sqrt(b * b - 4 * a * c)) / (2 * a)
-        return (first_root > 0 || second_root > 0) && (c > 0)
-    }
-
-    const calculateYBounds = (type, coeffs) => {
-        const constant = (type === 'quadratic' ? coeffs[2] : coeffs[1]);
-        setYBounds([-constant, constant])
-    }
-
-    const navigate = useNavigate();
-    const handleStartLesson = () => {
-        const functionString = functionType === 'quadratic' ? `${a}x^2 + ${b}x + ${c}` : `${a}x + ${b}`;
-        console.log('function type is', functionType);
-        navigate('/lesson', { state: { functionType: functionType, functionString: functionString, xBounds: xBounds, yBounds: yBounds } });
-    }
-
-    return (
-        <>
-            <NavBar />
-            <div className="function-selector-screen">
-                <h1>Choose the type of equation</h1>
-                <input className="btn-check" type="radio" id="quadratic" name="functionType" value="quadratic" checked={functionType === 'quadratic'} onChange={(e) => setFunctionType(e.target.value)} />
-                <label htmlFor="quadratic" className="btn btn-success">Quadratic</label>
-                <input className="btn-check" type="radio" id="linear" name="functionType" value="linear" checked={functionType === 'linear'} onChange={(e) => setFunctionType(e.target.value)} />
-                <label htmlFor="linear" className="btn btn-success">Linear</label>
-                <h1>Enter the equation you want to plot</h1>
-                <StaticMathField>{'y ='}</StaticMathField>
-                <EditableMathField
-                    latex={a}
-                    onChange={(mathField) => {
-                        setA(mathField.latex())
-                    }}
-                />
-                <StaticMathField>{functionType === 'quadratic' ? 'x^2 +' : 'x +'}</StaticMathField>
-                <EditableMathField
-                    latex={b}
-                    onChange={(mathField) => {
-                        setB(mathField.latex())
-                    }}
-                />
-                {functionType === 'quadratic' &&
-                    <>
-                        <StaticMathField>{'x +'}</StaticMathField>
-                        <EditableMathField
-                            latex={c}
-                            onChange={(mathField) => {
-                                setC(mathField.latex())
-                            }}
-                        />
-                    </>}
-                <div id="graph"></div>
-                <div id="error"></div>
-                <button className="btn btn-primary" onClick={handleStartLesson} disabled={!isValidEquation(functionType, [a, b, c])}>Start Lesson</button>
-            </div>
-        </>
-    )
-}
+/* Routing */
 
 const router = createBrowserRouter([
     {
