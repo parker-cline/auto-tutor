47a6c2f69167f2ddefbc4f38024c1b636753c182
Parker Cline <parkerpawleysisland@gmail.com>
2023-04-01 21:40:27 +0800

    moved canvas.js into a components folder

--- src/components/canvas.js
+++ src/components/canvas.js
@@ -6,9 +6,9 @@ function CanvasEditor({ handleClearCanvas, handleChangeColor, handleUndoStroke }
     return (
         <>
             <button type="button" className="btn btn-success col-sm-2" onClick={handleClearCanvas}><i className="bi bi-trash"></i> Clear</button>
-            <button type="button" className="btn btn-dark col-sm-2" onClick={() => handleChangeColor('black')}><i class="bi bi-paint-bucket"></i> Change to Black</button>
-            <button type="button" className="btn btn-danger col-sm-2" onClick={() => handleChangeColor('red')}><i class="bi bi-paint-bucket"></i> Change to Red</button>
-            <button type="button" className="btn btn-primary col-sm-2" onClick={() => handleChangeColor('blue')}><i class="bi bi-paint-bucket"></i> Change to Blue</button>
+            <button type="button" className="btn btn-dark col-sm-2" onClick={() => handleChangeColor('black')}><i className="bi bi-paint-bucket"></i> Change to Black</button>
+            <button type="button" className="btn btn-danger col-sm-2" onClick={() => handleChangeColor('red')}><i className="bi bi-paint-bucket"></i> Change to Red</button>
+            <button type="button" className="btn btn-primary col-sm-2" onClick={() => handleChangeColor('blue')}><i className="bi bi-paint-bucket"></i> Change to Blue</button>
             <button type="button" className="btn btn-secondary col-sm-2" onClick={handleUndoStroke}><i className="bi-arrow-counterclockwise"></i> Undo</button>
         </>
     );
@@ -16,54 +16,62 @@ function CanvasEditor({ handleClearCanvas, handleChangeColor, handleUndoStroke }
 
 function DrawingCanvas() {
     const [drawing, setDrawing] = useState(false);
+
+    // these are references to the canvas and details about the canvas
     const canvasRef = useRef(null);
     const ctxRef = useRef(null);
-    const [undoSteps, setUndoSteps] = useState({});
-    const [undo, setUndo] = useState(0);
 
-    // keep track of stroke locations
+
+    const [undoSteps, setUndoSteps] = useState({}); // this is a list of points (and the color of the points) that are on this canvas
+    const [numSteps, setNumSteps] = useState(0); // this is the number of steps that have been taken
 
     const startDraw = ({ nativeEvent }) => {
-        const { offsetX, offsetY } = nativeEvent;
+        const { offsetX, offsetY } = nativeEvent; // x and y coordinates of the mouse
         ctxRef.current.beginPath();
+        // move
         ctxRef.current.moveTo(offsetX, offsetY);
         const temp = {
             ...undoSteps,
-            [undo + 1]: []
+            [numSteps + 1]: []
         };
-        temp[undo + 1].push(ctxRef.current.strokeStyle, { offsetX, offsetY });
+        // store the current point in an array
+        temp[numSteps + 1].push(ctxRef.current.strokeStyle, { offsetX, offsetY });
         setUndoSteps(temp);
-        setUndo(undo + 1);
+        setNumSteps(numSteps + 1);
         setDrawing(true);
     };
 
     const stopDraw = () => {
         ctxRef.current.closePath();
         setDrawing(false);
+        console.log(undoSteps);
     };
 
     const draw = ({ nativeEvent }) => {
         if (!drawing) return;
         const { offsetX, offsetY } = nativeEvent;
+        // draw a tiny line from the last point to the current point over the mouse cursor
         ctxRef.current.lineTo(offsetX, offsetY);
         ctxRef.current.stroke();
         const temp = {
             ...undoSteps
         };
-        temp[undo].push({ offsetX, offsetY });
+        // store the current point in an array
+        temp[numSteps].push({ offsetX, offsetY });
         setUndoSteps(temp);
     };
 
     const handleUndoStroke = () => {
         //https://stackoverflow.com/questions/64611155/canvas-freehand-drawing-undo-and-redo-functionality-in-reactjs
-        if (undo > 0) {
+        if (numSteps > 0) {
             const currentColor = ctxRef.current.strokeStyle;
 
             // clear canvas
             const canvas = canvasRef.current;
             ctxRef.current.clearRect(0, 0, canvas.width, canvas.height);
-            // redraw canvas
-            for (let i = 1; i < undo; i++) {
+
+            // redraw canvas for everything except the last stroke
+            for (let i = 1; i < numSteps; i++) {
                 const temp = undoSteps[i];
                 ctxRef.current.strokeStyle = temp[0];
                 ctxRef.current.beginPath();
@@ -81,9 +89,9 @@ function DrawingCanvas() {
 
             const temp = {
                 ...undoSteps,
-                [undo]: []
+                [numSteps]: []
             };
-            setUndo(undo - 1);
+            setNumSteps(numSteps - 1);
             setUndoSteps(temp);
         }
     };
@@ -91,7 +99,7 @@ function DrawingCanvas() {
     const handleClearCanvas = () => {
         const canvas = canvasRef.current;
         setUndoSteps({});
-        setUndo(0);
+        setNumSteps(0);
         ctxRef.current.clearRect(0, 0, canvas.width, canvas.height);
     }
 
@@ -100,12 +108,13 @@ function DrawingCanvas() {
     }
 
     useEffect(() => {
+        // set width and height of canvas to match the window size
         const canvas = canvasRef.current;
-        // For supporting computers with higher screen densities, we double the screen density
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight / 1.5;
         canvas.style.width = `${window.innerWidth}px`;
         canvas.style.height = `${window.innerHeight / 1.5}px`;
+
         // Setting the context to enable us draw
         const ctx = canvas.getContext('2d');
         ctx.lineCap = 'round';
